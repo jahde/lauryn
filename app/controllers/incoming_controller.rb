13d2e375bdee20059bb9129e70a2c1434de0d2f0
jahde <jahde.hiywan@gmail.com>
2015-05-04 08:42:59 -0400

    added xml response

--- app/controllers/incoming_controller.rb
+++ app/controllers/incoming_controller.rb
@@ -27,26 +27,38 @@ class IncomingController < ApplicationController
     body = params[:Body]
 
     @twiml = Twilio::TwiML::Response.new do |r|
-      # if body == "hello"
-      #     r.Message "Hi!"
-      # else
-      #     r.Message "Wagwan star! I got a text from you."
-      # end
-      r.Message "What up bruh."
+      if body == "Lauryn, what is my current location?"
+          r.Message "Ready Or Not, Here I Come, You Can't Hide.
+Gonna Find You and Take it Slowly"
+      else
+          r.Message "You might win some but you just lost one"
+      end
+      # r.Message "What up bruh."
     end
 
     render 'send_message.xml.erb', :content_type => 'text/xml'
   end
 
 
-  private
+  # private
 
-  def client
-    twilio_phone_number = "9734594512"
-    account_sid = 'AC963408da3e7c1fbc06511e5cda6a0e9e'
-    auth_token = '0c2ce2f9669a32c23a9f82bc44b30b53'
+  # def client
+  #   twilio_phone_number = "9734594512"
+  #   account_sid = 'AC963408da3e7c1fbc06511e5cda6a0e9e'
+  #   auth_token = '0c2ce2f9669a32c23a9f82bc44b30b53'
 
-    # set up a client to talk to the Twilio REST API
-    @client = Twilio::REST::Client.new account_sid, auth_token
-  end
+  #   # set up a client to talk to the Twilio REST API
+  #   @client = Twilio::REST::Client.new account_sid, auth_token
+  # end
 end
+
+# What is the meaning of life?
+# Oooh La La La La La La Lalala La Laaah
+# A sweeeeet thing
+
+# What is my current location?
+# Ready Or Not, Here I Come, You Can't Hide
+# Gonna Find You and Take it Slowly
+
+# other question:
+# You might win some but you just lost one
