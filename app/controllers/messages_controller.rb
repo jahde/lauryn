e73c7c5bb5151acb6468bc3dcf1abbaba069f475
jahde <jahde.hiywan@gmail.com>
2015-05-03 22:30:13 -0400

    test commit

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,3 +1,5 @@
+Ngrok::Tunnel.start
+
 class MessagesController < ApplicationController
   def index
     client
@@ -11,6 +13,16 @@ class MessagesController < ApplicationController
 
     # set up a client to talk to the Twilio REST API
     @client = Twilio::REST::Client.new account_sid, auth_token
-    @client
   end
 end
+
+# What is the meaning of life?
+# Oooh La La La La La La Lalala La Laaah
+# A sweeeeet thing
+
+# What is my current location?
+# Ready Or Not, Here I Come, You Can't Hide
+# Gonna Find You and Take it Slowly
+
+# other question:
+# You might win some but you just lost one
